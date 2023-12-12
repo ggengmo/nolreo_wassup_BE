6027f1cd9470d1272ce1da1adca0ce0960679396
byeongheon.hwang <elwl5515@gmail.com>
2023-12-12 14:12:49 +0900

    :memo: Doc: README.md wireframe 추가
    - 수정 파일
    .\README.md
    
    - 수정 사항
    완성된 와이어프레임 이미지 README.md 적용

--- lodging/models.py
+++ lodging/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
-# Create your models here.
+class MainLocation(models.Model):
+    '''
+    메인 지역 모델
+    '''
+    address = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.address
+    
+
+class SubLocation(models.Model):
+    '''
+    서브 지역 모델
+    '''
+    address = models.CharField(max_length=100)
+    main_location = models.ForeignKey('MainLocation', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.address
+    
+
+class Lodging(models.Model):
+    '''
+    숙소 모델
+    '''
+    name = models.CharField(max_length=100)
+    intro = models.TextField()
+    notice = models.TextField()
+    info = models.TextField()
+    sub_location = models.ForeignKey('SubLocation', on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+
+class RoomType(models.Model):
+    '''
+    객실 타입 모델
+    '''
+    name = models.CharField(max_length=100)
+    capacity = models.IntegerField()
+    lodging = models.ForeignKey('Lodging', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
+class RoomImage(models.Model):
+    '''
+    객실 이미지 모델
+    '''
+    image = models.CharField(max_length=200)
+    room_type = models.ForeignKey('RoomType', on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.image_url
+
+
+class Amenity(models.Model):
+    '''
+    편의시설 모델
+    '''
+    name = models.CharField(max_length=100)
+    lodging = models.ForeignKey('Lodging', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
