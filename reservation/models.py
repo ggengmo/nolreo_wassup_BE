e9884e03c460f56ebb4e04f52799f9d709509c73
LeeCH <ckdgus2323@naver.com>
2023-12-12 15:55:10 +0900

    :bug: Fix: reservation models.py 컬럼명 수정

--- reservation/models.py
+++ reservation/models.py
@@ -6,7 +6,7 @@ class Reservation(models.Model):
     '''
     start_at = models.DateTimeField(null=True, blank=True)
     end_at = models.DateTimeField(null=True, blank=True)
-    type = models.CharField(max_length=100)
+    reservation_type = models.CharField(max_length=100)
     user = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE, related_name='reservations')
     lodging = models.ForeignKey('lodging.Lodging', on_delete=models.CASCADE, null=True, blank=True, related_name='reservations')
     bus = models.ForeignKey('traffic.Bus', on_delete=models.CASCADE, null=True, blank=True, related_name='reservations')
@@ -15,4 +15,3 @@ class Reservation(models.Model):
 
     def __str__(self):
         return f'이름: {self.user}, 예약일: {self.start_at}, 예약종료일: {self.end_at}'
-        
