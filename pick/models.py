100281b5bb314bcd49ab3fa5769cbbfdf603401d
ggengmo <rudah365dlf@gmail.com>
2023-12-12 15:30:16 +0900

    :sparkles: Feat: 찜 기능 테이블 모델
    - 수정 파일
    .\pick\models.py
    
    - 추가 사항
    찜 관련 테이블 모델 추가(숙소, 버스, 기차, 렌트)

--- pick/models.py
+++ pick/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Pick(models.Model):
+    '''
+    사용자 찜 모델
+    '''
+    lodging_id = models.ForeignKey('lodging.Lodging', on_delete=models.CASCADE, null=True, blank=True, related_name='lodging_pick')
+    bus_id = models.ForeignKey('traffic.Bus', on_delete=models.CASCADE, null=True, blank=True, related_name='bus_pick')
+    train_id = models.ForeignKey('traffic.Train', on_delete=models.CASCADE, null=True, blank=True, related_name='train_pick')
+    rental_car_id = models.ForeignKey('traffic.RentalCar', on_delete=models.CASCADE, null=True, blank=True, related_name='rental_car_pick')
+    user_id = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE)
