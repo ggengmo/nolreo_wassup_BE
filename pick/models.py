3c449b1841c6239a16fcd97281ebddafd39c399c
ggengmo <rudah365dlf@gmail.com>
2023-12-12 15:47:43 +0900

    :bug: Fix: 찜 유형 추가
    - 수정 파일
    .\pick\models.py
    
    - 수정 사항용
    Pick 모델 찜 유형 추가

--- pick/models.py
+++ pick/models.py
@@ -4,8 +4,12 @@ class Pick(models.Model):
     '''
     사용자 찜 모델
     '''
-    lodging_id = models.ForeignKey('lodging.Lodging', on_delete=models.CASCADE, null=True, blank=True, related_name='lodging_pick')
-    bus_id = models.ForeignKey('traffic.Bus', on_delete=models.CASCADE, null=True, blank=True, related_name='bus_pick')
-    train_id = models.ForeignKey('traffic.Train', on_delete=models.CASCADE, null=True, blank=True, related_name='train_pick')
-    rental_car_id = models.ForeignKey('traffic.RentalCar', on_delete=models.CASCADE, null=True, blank=True, related_name='rental_car_pick')
-    user_id = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE)
+    lodging= models.ForeignKey('lodging.Lodging', on_delete=models.CASCADE, null=True, blank=True, related_name='lodging_pick')
+    bus = models.ForeignKey('traffic.Bus', on_delete=models.CASCADE, null=True, blank=True, related_name='bus_pick')
+    train = models.ForeignKey('traffic.Train', on_delete=models.CASCADE, null=True, blank=True, related_name='train_pick')
+    rental_car = models.ForeignKey('traffic.RentalCar', on_delete=models.CASCADE, null=True, blank=True, related_name='rental_car_pick')
+    user = models.ForeignKey('account.CustomUser', on_delete=models.CASCADE, related_name='picks')
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.type}'
