4ae5bc8ebdaab196eecfc65151f07096c9cd7c96
ggengmo <rudah365dlf@gmail.com>
2023-12-13 11:13:11 +0900

    :sparkles: Feat: 버스 생성 테스트 추가
    - 수정 파일
    .\traffic\tests.py
    
    - 수정 사항
    출발시간이 현재 시간과 같을 경우 테스트 추가
    도착시간이 현재 시간과 같을 경우 테스트 추가

--- traffic/tests.py
+++ traffic/tests.py
@@ -65,4 +65,28 @@ class TestTrafficCase(TestCase):
         }
         response = self.client.post('/traffic/bus/', bus_data, format='json')
         self.assertEqual(response.status_code, 400)
+        # 비정상 처리 테스트 - 출발시간이 현재시간과 같을 경우
+        current_time = '2023-12-12 10:00:00'
+        print(current_time)
+        bus_data = {
+            'depart_point': '서울',
+            'dest_point': '부산',
+            'depart_time': '2023-12-12 10:00:00',
+            'arrival_time': '2023-12-12 10:30:00',
+            'num': '1234',
+        }
+        response = self.client.post('/traffic/bus/', bus_data, format='json')
+        self.assertEqual(response.status_code, 400)
+        # 비정상 처리 테스트 - 도착시간이 현재시간과 같을 경우
+        current_time = '2023-12-12 10:00:00'
+        print(current_time)
+        bus_data = {
+            'depart_point': '서울',
+            'dest_point': '부산',
+            'depart_time': '2023-12-12 09:00:00',
+            'arrival_time': '2023-12-12 10:00:00',
+            'num': '1234',
+        }
+        response = self.client.post('/traffic/bus/', bus_data, format='json')
+        self.assertEqual(response.status_code, 400)
         print('-- 버스 생성 테스트 END --')
