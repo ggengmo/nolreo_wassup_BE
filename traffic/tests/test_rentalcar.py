7f8719b3914680e7f52ca099c3bf96f1fe7b5339
ggengmo <rudah365dlf@gmail.com>
2023-12-14 14:43:37 +0900

    :sparkles: Feat: 렌트카 CRUD 기능 및 테스트
    - 생성 파일
    .\traffic\tests\tests_rentalcar.py
    
    - 수정 파일
    .\traffic\serializers.py
    .\traffic\models.py
    .\traffic\urls.py
    .\traffic\views.py
    
    - 개발 사항
    기차 CRUD 기능과 테스트 코드
    
    - 테스트 사항
            - 렌트카 생성 테스트(관리자)
            - 비정상 처리 테스트 - 차량 번호가 중복일 경우
    	- 비정상 처리 테스트 - 차량 모델이 없을 경우
    	- 비정상 처리 테스트 - 차량 지역이 없을 경우
    	- 비정상 처리 테스트 - 차량 번호가 없을 경우
    	- 비정상 처리 테스트 - 차량 가격이 없을 경우
    	- 렌트카 생성 테스트(유저)
    	- 렌트카 리스트 테스트
    	- 렌트카 조회 테스트
    	- 렌트카 수정 테스트(관리자)
    	- 비정상 처리 테스트 - 존재하지 않는 렌트카 수정 시도
    	- 비정상 처리 테스트 - 차량 번호가 중복일 경우
    	- 비정상 처리 테스트 - 차량 모델이 없을 경우
    	- 비정상 처리 테스트 - 차량 지역이 없을 경우
    	- 렌트카 수정 테스트(유저)
    	- 렌트카 삭제 테스트(관리자)
    	- 렌트카 삭제 테스트(유저)
    
    - 수정 사항
    models.py 에러 메세지 수정

---
+++ traffic/tests/test_rentalcar.py
@@ -0,0 +1,224 @@
+from django.test import TestCase
+from rest_framework.test import APIClient
+
+from account.models import CustomUser as User
+from traffic.models import RentalCar
+
+class TestRentalCarCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.admin = User.objects.create_superuser(
+            email = 'test1@gmail.com',
+            password = 'testtest1@',
+        )
+        self.user = User.objects.create_user(
+            email = 'test2@gmail.com',
+            password = 'testtest2@',
+            nickname = 'test2',
+        )
+        # 렌트카 데이터 생성
+        for i in range(10):
+            RentalCar.objects.create(
+                model = '소나타',
+                area = '서울',
+                num = f'{12341 + i}',
+                price = '10000',
+            )
+
+    def test_RentalCar_create_admin(self):
+        '''
+        렌트카 생성 테스트(관리자)
+        '''
+        print('-- 렌트카 생성 테스트(관리자) BEGIN --')
+        # 정상 처리 테스트
+        self.client.force_authenticate(user=self.admin)
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '서울',
+            'num': '99599',
+            'price': '10000',
+        }
+        response = self.client.post('/traffic/rentalcar/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 201)
+
+        # 비정상 처리 테스트 - 차량 번호가 중복일 경우
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '서울',
+            'num': '99599',
+            'price': '10000',
+        }
+        response = self.client.post('/traffic/rentalcar/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 400)
+        # 비정상 처리 테스트 - 차량 모델이 없을 경우
+        RentalCar_data = {
+            'model': '',
+            'area': '서울',
+            'num': '99919',
+            'price': '10000',
+        }
+        response = self.client.post('/traffic/rentalcar/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 400)
+        # 비정상 처리 테스트 - 차량 지역이 없을 경우
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '',
+            'num': '99929',
+            'price': '10000',
+        }
+        response = self.client.post('/traffic/rentalcar/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 400)
+        # 비정상 처리 테스트 - 차량 번호가 없을 경우
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '서울',
+            'num': '',
+            'price': '10000',
+        }
+        response = self.client.post('/traffic/rentalcar/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 400)
+        # 비정상 처리 테스트 - 차량 가격이 없을 경우
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '서울',
+            'num': '99939',
+            'price': '',
+        }
+        response = self.client.post('/traffic/rentalcar/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 400)
+        print('-- 렌트카 생성 테스트(관리자) END --')
+
+    def test_RentalCar_create_user(self):
+        '''
+        렌트카 생성 테스트(유저)
+        '''
+        print('-- 렌트카 생성 테스트(유저) BEGIN --')
+        # 정상 처리 테스트
+        self.client.force_login(user=self.user)
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '서울',
+            'num': '99599',
+            'price': '10000',
+        }
+        response = self.client.post('/traffic/rentalcar/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 401)
+        print('-- 렌트카 생성 테스트(유저) END --')
+
+    def test_RentalCar_list(self):
+        '''
+        렌트카 리스트 테스트
+        '''
+        print('-- 렌트카 리스트 테스트 BEGIN --')
+        # 정상 처리 테스트
+        response = self.client.get('/traffic/rentalcar/')
+        self.assertEqual(response.status_code, 200)
+        print('-- 렌트카 리스트 테스트 END --')
+
+    def test_RentalCar_retrieve(self):
+        '''
+        렌트카 조회 테스트
+        '''
+        print('-- 렌트카 조회 테스트 BEGIN --')
+        # 정상 처리 테스트
+        response = self.client.get('/traffic/rentalcar/1/')
+        self.assertEqual(response.status_code, 200)
+        # 비정상 처리 테스트 - 존재하지 않는 렌트카 조회 시도
+        response = self.client.get('/traffic/rentalcar/100/')
+        self.assertEqual(response.status_code, 404)
+        print('-- 렌트카 조회 테스트 END --')
+
+    def test_RentalCar_update_admin(self):
+        '''
+        렌트카 수정 테스트(관리자)
+        '''
+        print('-- 렌트카 수정 테스트(관리자) BEGIN --')
+        # 정상 처리 테스트
+        self.client.force_authenticate(user=self.admin)
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '서울',
+            'num': '99599',
+            'price': '10000',
+        }
+        response = self.client.put('/traffic/rentalcar/1/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        # 비정상 처리 테스트 - 존재하지 않는 렌트카 수정 시도
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '서울',
+            'num': '99599',
+            'price': '10000',
+        }
+        response = self.client.put('/traffic/rentalcar/100/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 404)
+        # 비정상 처리 테스트 - 차량 번호가 중복일 경우
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '서울',
+            'num': '12345',
+            'price': '10000',
+        }
+        response = self.client.put('/traffic/rentalcar/1/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 400)
+        # 비정상 처리 테스트 - 차량 모델이 없을 경우
+        RentalCar_data = {
+            'model': '',
+            'area': '서울',
+            'num': '99919',
+            'price': '10000',
+        }
+        response = self.client.put('/traffic/rentalcar/1/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 400)
+        # 비정상 처리 테스트 - 차량 지역이 없을 경우
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '',
+            'num': '99929',
+            'price': '10000',
+        }
+        response = self.client.put('/traffic/rentalcar/1/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 400)
+        print('-- 렌트카 수정 테스트(관리자) END --')
+
+    def test_RentalCar_update_user(self):
+        '''
+        렌트카 수정 테스트(유저)
+        '''
+        print('-- 렌트카 수정 테스트(유저) BEGIN --')
+        # 정상 처리 테스트
+        self.client.force_login(user=self.user)
+        RentalCar_data = {
+            'model': '소나타',
+            'area': '서울',
+            'num': '99599',
+            'price': '10000',
+        }
+        response = self.client.put('/traffic/rentalcar/1/', RentalCar_data, format='json')
+        self.assertEqual(response.status_code, 401)
+        print('-- 렌트카 수정 테스트(유저) END --')
+
+    def test_RentalCar_destroy_admin(self):
+        '''
+        렌트카 삭제 테스트(관리자)
+        '''
+        print('-- 렌트카 삭제 테스트(관리자) BEGIN --')
+        # 정상 처리 테스트
+        self.client.force_authenticate(user=self.admin)
+        response = self.client.delete('/traffic/rentalcar/1/')
+        self.assertEqual(response.status_code, 204)
+        # 비정상 처리 테스트 - 존재하지 않는 렌트카 삭제 시도
+        response = self.client.delete('/traffic/rentalcar/100/')
+        self.assertEqual(response.status_code, 404)
+        print('-- 렌트카 삭제 테스트(관리자) END --')
+
+    def test_RentalCar_destroy_user(self):
+        '''
+        렌트카 삭제 테스트(유저)
+        '''
+        print('-- 렌트카 삭제 테스트(유저) BEGIN --')
+        # 비정상 처리 테스트 - 유저가 렌트카 삭제 시도
+        self.client.force_login(user=self.user)
+        response = self.client.delete('/traffic/rentalcar/1/')
+        self.assertEqual(response.status_code, 401)
+        print('-- 렌트카 삭제 테스트(유저) END --')
--- traffic/views.py
+++ traffic/views.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 from rest_framework import viewsets
-from .serializers import BusSerializer, TrainSerializer
-from .models import Bus, Train
+from .serializers import BusSerializer, TrainSerializer, RentalCarSerializer
+from .models import Bus, Train, RentalCar
 
 
 class BusViewSet(viewsets.ModelViewSet):
@@ -25,6 +25,21 @@ class TrainViewSet(viewsets.ModelViewSet):
     '''
     queryset = Train.objects.all()
     serializer_class = TrainSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'destroy']:
+            permission_classes = [IsAdminUser]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+    
+
+class RentalCarViewSet(viewsets.ModelViewSet):
+    '''
+    렌트카 생성 API
+    '''
+    queryset = RentalCar.objects.all()
+    serializer_class = RentalCarSerializer
 
     def get_permissions(self):
         if self.action in ['create', 'update', 'destroy']:
