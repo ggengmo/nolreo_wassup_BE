caf48698f4007cc48712746ab0d8f2d5b06c7ae7
LeeCH <ckdgus2323@naver.com>
2023-12-15 18:03:07 +0900

    :sparkles: Feat: 숙소 리뷰 API 개발 및 테스트코드 작성
    - 추가 내용
    	- 숙소 리뷰 API 개발
    	- 숙소 리뷰 API TEST 코드 작성 및 확인

--- lodging/views.py
+++ lodging/views.py
@@ -1,4 +1,5 @@
 # Rest Framework
+from typing import Any
 from rest_framework import viewsets, generics, mixins
 from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 
@@ -6,10 +7,12 @@ from rest_framework.permissions import IsAdminUser, IsAuthenticated, AllowAny
 from .models import (
     Lodging,
     LodgingImage,
+    LodgingReview,
     )
 from .serializers import (
     LodgingSerializer,
     LodgingImageSerializer,
+    LodgingReviewSerializer,
     )
 
 class LodgingImageViewSet(viewsets.ModelViewSet):
@@ -46,3 +49,33 @@ class LodgingViewSet(viewsets.ModelViewSet):
         else:
             permission_classes = [AllowAny]
         return [permission() for permission in permission_classes]
+
+class LodgingReviewViewSet(viewsets.ModelViewSet):
+    '''
+    숙소 리뷰 ViewSet
+    '''
+    queryset = LodgingReview.objects.all()
+    serializer_class = LodgingReviewSerializer
+    action_map = {
+        'put': 'partial_update',
+    }
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'destroy']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+    
+    def create(self, request, *args, **kwargs):
+        '''
+        숙소 리뷰 생성 API
+        '''
+
+        return super().create(request, *args, **kwargs)
+
+lodging_review = LodgingReviewViewSet.as_view({
+    'post': 'create',
+    'get': 'list',
+    'patch': 'partial_update',
+})
