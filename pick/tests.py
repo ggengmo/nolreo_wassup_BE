fee5c3a0bb2f10db7a3b6c550b31345990605fa6
byeongheon.hwang <elwl5515@gmail.com>
2023-12-16 22:17:14 +0900

    :sparkles: Feat: 버스 찜 목록 API
    - 개발 사항
    	- 로그인한 사용자 대상 본인의 버스 찜 목록 조회 기능 개발
    
    - 테스트 사항
    	- 미로그인 상태에서 버스 찜 리스트 조회 요청 테스트
            - 로그인 상태에서 본인 버스 찜 리스트(찜 목록이 있는 경우) 조회 요청 테스트
            - 로그인 상태에서 본인 버스 찜 리스트(찜 목록이 없는 경우) 조회 요청 테스트
    
    - 테스트 명령어
    	- `python manage.py test pick.tests.TestPickBus.test_pick_bus_list`

--- pick/tests.py
+++ pick/tests.py
@@ -291,3 +291,70 @@ class TestPickBus(TestCase):
         self.assertEqual(response.data['message'], '이미 찜한 버스입니다.')
         print('-- 버스 찜 생성 테스트 END --')
 
+    def test_pick_bus_list(self):
+        '''
+        버스 찜 리스트 조회 테스트
+        1. 미로그인 상태에서 버스 찜 리스트 조회 요청 테스트
+        2. 로그인 상태에서 본인 버스 찜 리스트(찜 목록이 있는 경우) 조회 요청 테스트
+        3. 로그인 상태에서 본인 버스 찜 리스트(찜 목록이 없는 경우) 조회 요청 테스트
+        '''
+        print('-- 버스 찜 리스트 조회 테스트 BEGIN --')
+        data = {
+            'user':1,
+            'pick_type': 'BU',
+        }
+        for i in range(1, 6):
+            self.client.post(
+                '/pick/bus/',
+                data={
+                    'bus': i,
+                    'pick_type': 'BU',
+                    'user': 1,},
+                HTTP_AUTHORIZATION=f'Bearer {self.access_token}',
+                format='json')
+        # 미로그인 상태에서 버스 찜 리스트 조회 요청
+        response = self.client.get(
+            '/pick/bus/',
+            data=data,
+            format='json')
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.data['detail'], '로그인이 필요합니다.')
+
+        # 로그인 상태에서 본인 버스 찜 리스트(찜 목록이 있는 경우) 조회 요청
+        response = self.client.get(
+            '/pick/bus/',
+            HTTP_AUTHORIZATION=f'Bearer {self.access_token}',
+            data=data,
+            format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 5)
+
+        # 로그인 상태에서 본인 버스 찜 리스트(찜 목록이 없는 경우) 조회 요청
+        data = {
+            'email': 'test1@gmail.com',
+            'username': 'test',
+            'nickname': 'test1',
+            'password': 'testtest1@',
+            'password2': 'testtest1@',
+        }
+        self.client.post(
+            '/account/signup/', 
+            data,
+            format='json')
+        data = {
+            'email': 'test1@gmail.com',
+            'password': 'testtest1@',
+        }
+        response = self.client.post(
+            '/account/login/',
+            data,
+            format='json')
+        access = response.data['access']
+        response = self.client.get(
+            '/pick/bus/',
+            HTTP_AUTHORIZATION=f'Bearer {access}',
+            data=data,
+            format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), 0)
+        print('-- 버스 찜 리스트 조회 테스트 END --')
--- pick/urls.py
+++ pick/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'pick'
 
 urlpatterns = [
-    path('lodging/', views.lodging_pick, name='lodging_pick'),
-    path('lodging/<int:pk>/', views.lodging_pick, name='lodging_pick_patch'),
-    path('bus/', views.bus_pick, name='bus_pick'),
+    path('lodging/', views.lodging_pick, name='lodging_pick_cl'),
+    path('lodging/<int:pk>/', views.lodging_pick, name='lodging_pick_d'),
+    path('bus/', views.bus_pick, name='bus_pick_cl'),
 ]
