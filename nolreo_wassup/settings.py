0dfb872e916bc4e54050725bf3b7b14d3adabacc
byeongheon.hwang <elwl5515@gmail.com>
2023-12-26 10:06:18 +0900

    :bug: Fix: static root 경로 변경

--- nolreo_wassup/settings.py
+++ nolreo_wassup/settings.py
@@ -98,7 +98,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_Root = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
